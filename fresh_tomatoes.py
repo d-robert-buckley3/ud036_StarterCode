3098470b4d2c56776b2d0f6b21776abff3790887
Robert Buckley <d.robert.buckley3@gmail.com>
2017-06-13 10:25:49 -0700

    Fix long movie titles issue
    Issue: If any movie had a title longer than a certain amount (~20) it would be printed within the movie tile on more than 1 line.  When this happens it throws off the alignment of the grid on the row below.
    
    Fix: Only use the first 19 characters of the movie title and add ellipses if the title length exceeds 20 characters.
    
    TODO: It would be nice to find a way to display the full title in some other way.  Ideas:
    * Display full title as tooltip on mouseover
    * Scroll full title on one line back and forth still showing only enough characters to fit on one line within the tile
    * Enlarge the tiles to fit 2 lines of text
    * Dynamically increase the size of all tiles when a single one needs to increase its size in order to maintain proper grid alignment (this one seems too invasive)
    * Change title display in place from shortened title to full title on mouseover (this one will possible break alignment while viewing the title and could make the grid unstable on mouseovers)

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -143,7 +143,7 @@ def create_movie_tiles_content(movies):
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
-            movie_title=movie.title,
+            movie_title=movie.title if (len(movie.title) < 20) else movie.title[:19] + '...',
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
