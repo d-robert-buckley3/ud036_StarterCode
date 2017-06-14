ecc66a42b4edcd412eab8f57b81916c0dbcab6d0
Robert Buckley <d.robert.buckley3@gmail.com>
2017-06-14 10:49:04 -0700

    Add tooltip function to display full titles
    Also externalized the HTML content so it
    no longer clutters the fresh_tomatoes.py
    script.  This required a new function to
    load the external HTML.

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,150 +1,39 @@
 # From github repo:
 #     https://github.com/adarsh0806/ud036_StarterCode
 # Used per recommendation by Udacity project instructions
+# Heavily modified to externalize the HTML used
+#   and to fix an issue with movie tile alignments
 
 import webbrowser
 import os
 import re
 
 
-# Styles and scripting for the page
-main_page_head = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+def load_html(filename):
+    """
+    Load HTML page content or templates from an external file
 
-    <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <style type="text/css" media="screen">
-        body {
-            padding-top: 80px;
-        }
-        #trailer .modal-dialog {
-            margin-top: 200px;
-            width: 640px;
-            height: 480px;
-        }
-        .hanging-close {
-            position: absolute;
-            top: -12px;
-            right: -12px;
-            z-index: 9001;
-        }
-        #trailer-video {
-            width: 100%;
-            height: 100%;
-        }
-        .movie-tile {
-            margin-bottom: 20px;
-            padding-top: 20px;
-        }
-        .movie-tile .tooltip {
-            visibility: hidden;
-            background-color: black;
-            color: #fff;
-            text-align: center;
-            padding: 5px 0;
-            border-radius: 6px;
-            position: absolute;
-            z-index: 1;
-        }
-        .movie-tile:hover {
-            background-color: #EEE;
-            cursor: pointer;
-        }
-        .movie-tile:hover .tooltip {
-            visibility: visible;
-        }
-        .scale-media {
-            padding-bottom: 56.25%;
-            position: relative;
-        }
-        .scale-media iframe {
-            border: none;
-            height: 100%;
-            position: absolute;
-            width: 100%;
-            left: 0;
-            top: 0;
-            background-color: white;
-        }
-    </style>
-    <script type="text/javascript" charset="utf-8">
-        // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
-        });
-        // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
-        });
-        // Animate in the movies when the page loads
-        $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
-            $(this).next("div").show("fast", showNext);
-          });
-        });
-    </script>
-</head>
-'''
+    filename (str): name of external file containing HTML content
+
+    Returns HTML content as a string
+    """
+    content = ""
+    with open(filename, 'r') as f:
+        content = f.read()
+
+    return content
+
+
+# Styles and scripting for the page
+main_page_head = load_html("fresh_tomatoes_main_page_html.txt")
 
 
 # The main page layout and title bar
-main_page_content = '''
-  <body>
-    <!-- Trailer Video Modal -->
-    <div class="modal" id="trailer">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
-            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
-          </a>
-          <div class="scale-media" id="trailer-video-container">
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Main Page Content -->
-    <div class="container">
-      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      {movie_tiles}
-    </div>
-  </body>
-</html>
-'''
+main_page_content = load_html("fresh_tomatoes_main_page_content_html.txt")
 
 
 # A single movie entry html template
-movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title_short}</h2>
-    <span class="tooltip">{movie_title}</span>
-</div>
-'''
+movie_tile_content = load_html("fresh_tomatoes_movie_tile_html.txt")
 
 
 def create_movie_tiles_content(movies):
@@ -162,7 +51,8 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
-            movie_title_short=movie.title if (len(movie.title) < 20) else movie.title[:19] + '...',
+            movie_title_short=(movie.title if (len(movie.title) < 20) 
+                                else movie.title[:19] + '...'),
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
